b9660c7e79aac9f14cf9e6939c4f005e2877a00b
SwarnyAppakkuddy <84567450+SwarnyAppakkuddy@users.noreply.github.com>
2023-06-05 13:52:30 +0200

    initial push, no major changes

--- Frontend/pages/areas/[id].vue
+++ Frontend/pages/areas/[id].vue
@@ -1,21 +1,40 @@
 <template>
-    <h1> {{ area.name }}</h1>
-    <div v-for="project in projects" :key="project.id">
-      <h2>{{ project.title }}</h2>
+    <div class="areas">
+
+        <h1> {{ area.name }}</h1>
+        <div v-for="project in projects" :key="project.id">
+            <h2>{{ project.title }}</h2>
+        </div>
     </div>
 </template>
+
+
 <script>
-    export default defineNuxtComponent({
-            async asyncData() {
-                const route = useRoute()
-                const areaPromise = $fetch(useRuntimeConfig().public.serverURL + '/areas/area=' + route.params.id)
-                const projectsPromise = $fetch(useRuntimeConfig().public.serverURL + '/projects/area=' + route.params.id )
-                const [area, projects] = await Promise.all([areaPromise, projectsPromise])
-                
-                return {
-                    area,
-                    projects,
-                }
-        },
-    })
+export default defineNuxtComponent({
+    async asyncData() {
+        const route = useRoute()
+        const areaPromise = $fetch(useRuntimeConfig().public.serverURL + '/areas/area=' + route.params.id)
+        const projectsPromise = $fetch(useRuntimeConfig().public.serverURL + '/projects/area=' + route.params.id)
+        const [area, projects] = await Promise.all([areaPromise, projectsPromise])
+
+        return {
+            area,
+            projects,
+        }
+    },
+})
 </script>
+<style>
+.areas {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    grid-gap: 20px;
+}
+
+.areas h1 {
+    font-size: xx-large;
+    border-bottom: 2px solid #FF7A00;
+    position: relative;
+}
+</style>
