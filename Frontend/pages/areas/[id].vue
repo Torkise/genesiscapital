fbd2e208d238295c42673b0cfb0e424b70d39b5d
SwarnyAppakkuddy <84567450+SwarnyAppakkuddy@users.noreply.github.com>
2023-06-05 21:49:59 +0200

    Revert "Create specific area pages"

--- Frontend/pages/areas/[id].vue
+++ Frontend/pages/areas/[id].vue
@@ -1,44 +1,21 @@
 <template>
-    <div class="areas">
-        <h1> {{ area.name }} </h1>
-        <div v-html="area.description"></div>
-        <project-box :title="'Current Projects in this area:'" :projects="projects" />
+    <h1> {{ area.name }}</h1>
+    <div v-for="project in projects" :key="project.id">
+      <h2>{{ project.title }}</h2>
     </div>
 </template>
-
-
 <script>
-import ProjectBox from '@/components/ProjectBox.vue';
-
-export default defineNuxtComponent({
-    async asyncData() {
-        const route = useRoute()
-        const areaPromise = $fetch(useRuntimeConfig().public.serverURL + '/areas/area=' + route.params.id)
-        const projectsPromise = $fetch(useRuntimeConfig().public.serverURL + '/projects/area=' + route.params.id)
-        const [area, projects] = await Promise.all([areaPromise, projectsPromise])
-
-
-        return {
-            area,
-            projects,
-        }
-    },
-})
+    export default defineNuxtComponent({
+            async asyncData() {
+                const route = useRoute()
+                const areaPromise = $fetch(useRuntimeConfig().public.serverURL + '/areas/area=' + route.params.id)
+                const projectsPromise = $fetch(useRuntimeConfig().public.serverURL + '/projects/area=' + route.params.id )
+                const [area, projects] = await Promise.all([areaPromise, projectsPromise])
+                
+                return {
+                    area,
+                    projects,
+                }
+        },
+    })
 </script>
-
-
-<style>
-.areas {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    grid-gap: 20px;
-    padding-left: 8rem;
-}
-
-.areas h1 {
-    font-size: xx-large;
-    border-bottom: 2px solid #FF7A00;
-    position: relative;
-}
-</style>
--- Frontend/pages/projects/index.vue
+++ Frontend/pages/projects/index.vue
@@ -1,107 +1,104 @@
 <script>
-export default defineNuxtComponent({
-  async asyncData() {
-    const route = useRoute()
-    const projects = await $fetch(useRuntimeConfig().public.serverURL + '/projects')
-    return {
-      projects
+    export default defineNuxtComponent({
+        async asyncData() {
+            const route = useRoute()
+            const projects = await $fetch(useRuntimeConfig().public.serverURL + '/projects')
+            return {
+                projects
+            }
     }
-  }
 })
 </script>
 <template>
-  <div class="projects">
-    <PageTitle title="A Look into Our Investments" />
-    <p>Welcome to Genesis Capital Projects page, where you can discover the latest investment opportunities and innovative
-      startups we are backing. Our team of experienced professionals is dedicated to identifying and supporting the most
-      promising ventures across a variety of sectors. Explore our portfolio and learn more about the exciting new
-      companies we are supporting.</p>
-    <div class="project-list">
-      <div v-for="project in projects" :key="project.id">
-        <div class="project">
-          <NuxtLink :to="'/projects/' + project.id">
-            <div class="project-area" :class="area.toLowerCase()" v-for="area in project.areas" :key="area">
-              {{ area }}
-            </div>
-            <h2>{{ project.title }}</h2>
-            <p>{{ project.shortDescription }}</p>
-            <p class="learnmore">Learn more...</p>
-          </NuxtLink>
+    <div class="projects">
+      <PageTitle title="A Look into Our Investments"/>
+      <p>Welcome to Genesis Capital Projects page, where you can discover the latest investment opportunities and innovative startups we are backing. Our team of experienced professionals is dedicated to identifying and supporting the most promising ventures across a variety of sectors. Explore our portfolio and learn more about the exciting new companies we are supporting.</p>
+      <div class="project-list">
+        <div v-for="project in projects" :key="project.id">
+          <div class="project">
+            <NuxtLink :to="'/projects/' + project.id">
+              <div class="project-area" :class="area.toLowerCase()" v-for="area in project.areas" :key="area"> 
+                {{ area }}
+              </div>
+              <h2>{{ project.title }}</h2>
+              <p>{{ project.shortDescription }}</p>
+              <p class="learnmore">Learn more...</p>
+            </NuxtLink>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</template>
+  </template>
   
-<style>
-.projects {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  grid-gap: 20px;
-}
+  <style>
+    .projects {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      grid-gap: 20px;
+    }
 
-
-.project-list {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 2rem;
-}
-
-.project {
-  width: 30rem;
-  height: 10rem;
-  padding: 2rem;
-  border: 2px solid #ddd;
-  transition: all 0.3s cubic-bezier(.25, .8, .25, 1);
-  border-radius: 5px;
-}
-
-.project:hover {
-  transform: translateY(-10px);
-  box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1), 0 5px 15px rgba(0, 0, 0, 0.07);
-  cursor: pointer;
-}
-
-.project-area {
-  display: inline-block;
-  margin-right: 0.5rem;
-  margin-bottom: 0.5rem;
-  padding: 0.25rem 1rem;
-  border-radius: 1rem 1rem 1rem 1rem;
-  color: #fff;
-  text-transform: capitalize;
-}
-
-.project-area.healthcare {
-  background-color: #ffd388;
-}
-
-.project-area.energy {
-  background-color: #68b56b;
-}
-
-.project-area.technology {
-  background-color: #9ab8ff;
-}
-
-.project-area.sustainability {
-  background-color: #9d64f8;
-}
-
-.project h2 {
-  font-size: 1.5rem;
-  margin-top: 0;
-}
-
-.project p {
-  font-size: 1rem;
-}
-
-.learnmore {
-  margin-top: 1rem;
-  margin-left: 0.5rem;
-  color: #979797;
-}
-</style>
+    
+    .project-list {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 2rem;
+    }
+    
+    .project {
+      width: 30rem;
+      height: 10rem;
+      padding: 2rem;
+      border: 2px solid #ddd;
+      transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+      border-radius: 5px;
+    }
+  
+    .project:hover {
+      transform: translateY(-10px);
+      box-shadow: 0 15px 30px rgba(0, 0, 0, 0.1), 0 5px 15px rgba(0, 0, 0, 0.07);
+      cursor: pointer;
+    }
+  
+    .project-area {
+      display: inline-block;
+      margin-right: 0.5rem;
+      margin-bottom: 0.5rem;
+      padding: 0.25rem 1rem;
+      border-radius: 1rem 1rem 1rem 1rem;
+      color: #fff;
+      text-transform: capitalize;
+    }
+    
+    .project-area.healthcare {
+      background-color: #ffd388;
+    }
+    
+    .project-area.energy {
+      background-color: #68b56b;
+    }
+    
+    .project-area.technology {
+      background-color: #9ab8ff;
+    }
+  
+    .project-area.sustainability {
+      background-color: #9d64f8;
+    }
+  
+    .project h2 {
+      font-size: 1.5rem;
+      margin-top: 0;
+    }
+    
+    .project p {
+      font-size: 1rem;
+    }
+  
+    .learnmore {
+      margin-top: 1rem;
+      margin-left: 0.5rem;
+      color: #979797;
+    }
+  </style>
   
