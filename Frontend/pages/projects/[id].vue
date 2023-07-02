f4a3bf5429dc0a21df1f52b42748eb2098db6db0
Andreas <andreas.bo@hotmail.no>
2023-07-02 18:52:22 +0200

    project page ++

--- Frontend/pages/projects/[id].vue
+++ Frontend/pages/projects/[id].vue
@@ -1,147 +1,118 @@
 <template>
     <div class="project-div">
-        <pagetitle>{{ project.title }}</pagetitle>
-        <div class="project-content">
-            <p v-html="project.longDescription"></p>
-            <img class="project-img"
-                src="https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt="">
-        </div>
-        <div class="links-container">
-            <div class="links-areas">
-                <div class="project-areas" v-for="(area, key) in areas" :key="key">
-                    <h2>
-                        <NuxtLink :to="'/areas/' + area.id" :class="area.name">
-                            {{ area.name }}
-                        </NuxtLink>
-                    </h2>
-                </div>
-            </div>
-            <div class="links-supervisor">
-                <NuxtLink :to="'/employees/' + employee.id">
-                    <h2 class="supervisor">Supervisor: {{ employee.name }}</h2>
-                </NuxtLink>
-            </div>
-        </div>
+      <pagetitle>{{ project.title }}</pagetitle>
+      <div class="project-area" :class="area.name.toLowerCase()" v-for="area in areas">
+        <NuxtLink :to="'/areas/' + area.id"> 
+            {{ area.name }}
+        </NuxtLink>
+      </div>
+      <div class="project-content">
+        <p v-html="project.longDescription"></p>
+        <!-- <img class="project-img"
+             src="https://images.pexels.com/photos/6153354/pexels-photo-6153354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+             alt=""> -->
+        <img class="project-img" :src="project.photo">
+      </div>
+      <div class="links-container">
+        Supervised by
+        <NuxtLink :to="'/employees/' + employee.id"> 
+          <img class="employee-img" :src="employee.photo"><br>
+        </NuxtLink>
+        {{ employee.name }} <br>
+        <i>{{ employee.title }}</i>
+      </div>
     </div>
-</template>
-
-<script>
-export default defineNuxtComponent({
+  </template>
+  
+  <script>
+  export default defineNuxtComponent({
     async asyncData() {
-        const route = useRoute();
-        const areaPromise = $fetch(
-            useRuntimeConfig().public.serverURL + '/areas/project/projectid=' + route.params.id
-        );
-        const supervisorPromise = $fetch(
-            useRuntimeConfig().public.serverURL + '/supervisor/' + route.params.id
-        );
-        const projectsPromise = $fetch(
-            useRuntimeConfig().public.serverURL + '/project/' + route.params.id
-        );
-        const [areas, project, employee] = await Promise.all([
-            areaPromise,
-            projectsPromise,
-            supervisorPromise,
-        ]);
-
-        return {
-            areas,
-            project,
-            employee,
-        };
+      const route = useRoute();
+      const areaPromise = $fetch(
+        useRuntimeConfig().public.serverURL + '/areas/project/projectid=' + route.params.id
+      );
+      const supervisorPromise = $fetch(
+        useRuntimeConfig().public.serverURL + '/supervisor/' + route.params.id
+      );
+      const projectsPromise = $fetch(
+        useRuntimeConfig().public.serverURL + '/project/' + route.params.id
+      );
+      const [areas, project, employee] = await Promise.all([
+        areaPromise,
+        projectsPromise,
+        supervisorPromise,
+      ]);
+  
+      return {
+        areas,
+        project,
+        employee,
+      };
     },
-});
-</script>
-
-<style>
-.project-img {
+  });
+  </script>
+  
+  <style>
+  .project-img {
     max-width: 50vw;
     max-height: 50vw;
-    margin-top: 0;
     align-self: center;
     border-radius: 2rem;
-}
-
-.project-div {
+  }
+  
+  .project-div {
     margin-left: 4rem;
     margin-right: 4rem;
     margin-bottom: 4rem;
-}
-
-.project-content {
+  }
+  
+  .project-content {
     display: flex;
     flex-direction: row;
-}
-
-.project-content p {
-    font-size: large;
-}
-
-.project-content img {
-    max-width: 35vw;
-    margin-left: 2rem;
-}
-
-.links-container {
-    margin-top: 2rem;
-    display: flex;
-    flex-direction: column;
-    background: linear-gradient(to right,  rgba(29, 110, 9, 0), rgba(29, 110, 9, 0.15));
-    width: 30vi;
-    padding: 2rem;
-    border-radius: 2rem;
-    margin-left: auto;
-    align-items: center;
-}
-
-.links-areas {
-    display: flex;
-    flex-wrap: wrap;
-}
-
-.links-container h2 {
-    color: #000000;
-    font-size: 16px;
-    margin: 0;
-}
-
-.project-areas {
-    border-radius: 1rem;
-    color: #fff;
-    cursor: pointer;
-    padding: 0.5rem 1rem;
-    margin: 0.5rem;
-    background-color: rgba(222,241,237,255);
-}
-
-.project-areas:hover {
-    background-color: #ffc492;
-}
-
-.links-supervisor {
-    margin-top: 1rem;
-}
-
-.supervisor {
-    color: #000000;
-    font-size: 18px;
-}
-
-@media (max-width: 768px) {
-  .project-content {
-    flex-direction: column;
-    padding-right: 0;
   }
   
   .project-content img {
-    margin: 2rem auto;
-    min-width: 60vw;
-    min-height: auto;
+    max-width: 35vw;
+    margin-left: 2rem;
+    margin-right: 2rem;
   }
+  
   .links-container {
+    margin-top: 2rem;
+    display: flex;
+    flex-direction: column;
+    width: 22vi;
+    padding: 2rem;
+    align-items: center;
+  }
+  
+  .employee-img {
+    width: 10vi;
+    border-radius: 50%;
+    margin-top: 1rem;
     margin-left: auto;
     margin-right: auto;
+    display: block;
   }
-}
-</style>
+  
+  @media (max-width: 768px) {
+    .project-content {
+      flex-direction: column;
+      padding-right: 2rem;
+    }
+    
+    .project-content img {
+      margin: 2rem auto;
+      min-width: 60vw;
+      min-height: auto;
+    }
+  
+    .links-container {
+      margin-top: 2rem;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 2rem auto;
+    }
+  }
+  </style>  
